b1ef6ca439632bf78762a63f4548d0c6e41f8dcf
Jaydeep Kulkarni <jay.kulkarni2005@gmail.com>
2026-04-11 22:46:30 +0530

    updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
 
     testImplementation(libs.junit)
 
-    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
 }
